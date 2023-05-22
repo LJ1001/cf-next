f5e9956cb3d96c158c3720b15e3c6099c3fdd608
Marcel Dambach <marcel-dambach@gmx.de>
2023-05-22 12:08:30 +0200

    Remove unused import

--- src/app/api/protected/route.ts
+++ src/app/api/protected/route.ts
@@ -1,6 +1,5 @@
 import { getAuth } from "@clerk/nextjs/server";
 
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { NextRequest, NextResponse } from "next/server";
 export const runtime = 'edge';
 
